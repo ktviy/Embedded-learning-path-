0d012c365dfedf905b3952be6474e804f56e2470
ktviy <Khongtuongvy09@gmail.com>
2025-12-02 21:30:11 -0500

    Implement pointer arithmetic in birthday function
    Added pointer arithmetic example for birthday function.

--- C-Snippets/01_Basic_Pointers/basic_pointer_arithmetic.c
+++ C-Snippets/01_Basic_Pointers/basic_pointer_arithmetic.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+// pointer - A variable that stores the memory address of another variable
+// Benefit: save memory by passing the address of a large data structure instead of copying the entire structure
+
+void birthday(int *age);
+
+int main() {
+    int age = 0;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    int *pAge = &age;
+
+    birthday(pAge);
+    
+    printf("Next year, you are %d years old", age);
+}
+
+void birthday(int *age){
+    // pass by reference
+    (*age)++;
+}
