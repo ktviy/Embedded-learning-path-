9410efd277dec3c952b22c7a38f557f340ae9f79
ktviy <Khongtuongvy09@gmail.com>
2025-12-06 15:29:14 -0500

    Fix memory allocation check and update data handling

--- C-Snippets/02_Advanced_Pointers/dynamic_array_malloc.c
+++ C-Snippets/02_Advanced_Pointers/dynamic_array_malloc.c
@@ -19,29 +19,29 @@ int main(){
     Sensor_record *data_buffer = (Sensor_record*)malloc(count * sizeof(Sensor_record));
 
     // Free the dynamic memory allocated for Sensor_record
-    if (data_buffer = NULL){
+    if (data_buffer == NULL){
         printf("Memory allocation failed.\n");
         return 1;
     } 
 
-    Sensor_record lists[count];
-    Sensor_record *sensor_data = &lists[0];
 
     for (int i = 0; i < count; i++){
         printf("Enter the information of Sensor %d\n", i + 1);
 
         printf("Name of Sensor: ");
-        fgets(lists[i].name_sensor, sizeof(lists[i].name_sensor), stdin);
-        lists[i].name_sensor[strlen(lists[i].name_sensor) - 1] = '\0';
+        fgets(data_buffer[i].name_sensor, sizeof(data_buffer[i].name_sensor), stdin);
+        data_buffer[i].name_sensor[strlen(data_buffer[i].name_sensor) - 1] = '\0';
 
         printf("ID of Sensor: ");
-        scanf("%d", &lists[i].id_sensor);
+        scanf("%d", &data_buffer[i].id_sensor);
 
         printf("Value of Sensor: ");
-        scanf("%f", &lists[i].value_sensor);
+        scanf("%f", &data_buffer[i].value_sensor);
+
+        getchar();
     }
 
-    sensor_info(sensor_data, count);
+    sensor_info(data_buffer, count);
 
     free(data_buffer); // returning the allocated memory back to the system
     data_buffer = NULL; // Avoid dangling pointer
@@ -58,7 +58,7 @@ void sensor_info(Sensor_record *sensor, int count){
         printf("Sensor %d\n", i + 1);
         printf("Name of Sensor: %s\n", sensor[i].name_sensor); // Using array notation
         printf("ID of Sensor: %d\n", (sensor + i)->id_sensor); // Using pointer arithmetic 
-        printf("Value of Sensor: %.2f\n", (*sensor).value_sensor); // Dereferencing pointer
+        printf("Value of Sensor: %.2f\n", (*(sensor+i)).value_sensor); // Dereferencing pointer
 
         printf("\n");
     }
