be5286a6d640691ce35392957b550a8ee3f87bc7
ktviy <Khongtuongvy09@gmail.com>
2025-12-14 12:03:07 -0500

    Implement Sensor_data class with constructor and destructor
    Added a class to manage sensor data with memory allocation and deallocation.

--- CPP-OOP-Embedded/01_C++_Simple_OOP/sensor_data_class.cpp
+++ CPP-OOP-Embedded/01_C++_Simple_OOP/sensor_data_class.cpp
@@ -1,1 +1,57 @@
+#include <iostream>
+#include <string>
 
+class Sensor_data{
+    private: 
+        std::string sensor_id;
+        float Reading;
+        int* data_buffer = nullptr;
+
+    public: 
+
+        // Create a constructor to initialize members and allocate memory
+        Sensor_data(const std::string& id, float value, int buffer_size){
+            sensor_id = id;
+            Reading = value; 
+
+            {
+                data_buffer = new int[buffer_size];
+                std::cout << "Constructor object " << sensor_id << "created. Allocated "
+                          << buffer_size * sizeof(int) << " bytes on Healp" << std::endl;
+                }
+            }
+
+            // Create destructor to free allocated memory
+            ~Sensor_data(){
+                if(data_buffer != nullptr){
+                    delete[] data_buffer;
+                    std::cout << "Destructor object " << sensor_id << " destroyed, memory freed." << std::endl;
+                }
+            }
+            
+            // Getter: allow read access to private member Reading
+            float getReading() const {
+                return Reading;
+            }
+
+            // Setter: allow controlled write access to private member Reading
+            void setReading(float new_value){
+                if (new_value >= 0.0){
+                    Reading = new_value;
+                }
+            }
+};
+
+int main(){
+
+    Sensor_data sensor1("Sensor A", 10.5, 10);
+    std::cout << "Initial reading: " << sensor1.getReading() << std::endl;
+
+    sensor1.setReading(11.0);
+    std::cout << "New reading: " << sensor1.getReading() << std::endl;
+
+    std::cout << "End of program. Destructors called automatically" << std::endl;
+
+    return 0;
+
+}
