8bf8b60931e396659277e349d6bb7f036ea82e03
ktviy <Khongtuongvy09@gmail.com>
2025-12-19 16:30:43 -0500

    Add multiplication assembly code in Mult.asm
    This assembly code computes the product of R0 and R1, storing the result in R2 using a loop.

---
+++ Computer-Architecture-Basics/04_ISA_and_Assembly/Mult.asm
@@ -0,0 +1,35 @@
+// Computes R0 * R1 and stores result in R2
+
+    // R2 = 0
+    @R2
+    M=0
+
+    // Sei i = R1
+    @R1
+    D=M
+    @i
+    M=D // D=i
+
+(LOOP)
+    // if counter == 0 goto END
+    @i
+    D=M
+    @END
+    D;JEQ
+
+    // R2 = R2 + R0
+    @R0
+    D=M 
+    @R2
+    M=D+M
+
+    // i--
+    @i
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
